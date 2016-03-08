f6471ccb5a5e17cffc0f7a2b749daf036dc7f847
sutong.st <sutong.st@alibaba-inc.com>
2016-03-08 17:41:46 +0800

    add

---
+++ src/test/java/com/ream/www/maventest/NumberTest.java
@@ -0,0 +1,14 @@
+package com.ream.www.maventest;
+
+import org.junit.Test;
+
+/**
+ * Created by tong on 2/25/16.
+ */
+public class NumberTest {
+
+    @Test
+    public void test(){
+//        System.out.print();
+    }
+}
--- src/test/java/com/ream/www/maventest/utils/CollectionUtilTest.java
+++ src/test/java/com/ream/www/maventest/utils/CollectionUtilTest.java
@@ -1,7 +1,11 @@
 package com.ream.www.maventest.utils;
 
+import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by tong on 1/22/16.
@@ -16,5 +20,19 @@ public class CollectionUtilTest {
                 return integer + integer2;
             }
         }));
+    }
+
+    @Test
+    public void testCollection(){
+        List<ArrayList<Long>> listList = Lists.newArrayList(Lists.newArrayList(1l, 2l), Lists.newArrayList(2l, 3l));
+        List<Long> longs = CollectionUtil.reduce(listList.iterator(), new ArrayList<Long>(),
+                new CollectionUtil.Function<ArrayList<Long>, ArrayList<Long>>() {
+            public ArrayList<Long> apply(ArrayList<Long> longs, ArrayList<Long> longs2) {
+                longs2.addAll(longs);
+                return longs2;
+            }
+        });
+
+        System.out.println(JSON.toJSONString(longs));
     }
 }
