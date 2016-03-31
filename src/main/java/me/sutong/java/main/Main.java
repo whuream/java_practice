ee3a64c7603ffdfd7a5e826cc5125d6a05fd8d6d
sutong.st <sutong.st@alibaba-inc.com>
2016-03-31 15:47:51 +0800

    add

--- src/main/java/me/sutong/java/main/Main.java
+++ src/main/java/me/sutong/java/main/Main.java
@@ -3,7 +3,7 @@ package me.sutong.java.main;
 public class Main
 {
     public static void main(String[] args){
-        System.out.print("test succeed\n");
+        System.out.print("Hello World!\n");
 
     }
 }
